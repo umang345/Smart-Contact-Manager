e2ba43086d9083f3019558b61bed99441f91904c
Umang <umangagarwal345@gmail.com>
2022-05-16 15:49:03 +0530

    Added User and Contact entities.

---
+++ smart-contact-manager/smart-contact-manager/src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+
+@Controller
+public class HomeController 
+{
+
+}
