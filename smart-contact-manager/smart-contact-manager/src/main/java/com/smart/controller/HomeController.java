d7dd6f2f61620090cdff904570dd06d3f0f37339
Umang <umangagarwal345@gmail.com>
2022-05-16 18:24:06 +0530

    Designed home page

--- smart-contact-manager/smart-contact-manager/src/main/java/com/smart/controller/HomeController.java
+++ smart-contact-manager/smart-contact-manager/src/main/java/com/smart/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.dao.UserRepository;
@@ -11,5 +13,24 @@ import com.smart.entities.User;
 @Controller
 public class HomeController 
 {
-
+	@RequestMapping("/")
+	public String home(Model model)
+	{
+		 model.addAttribute("title","Home - Smart Contact Manager");
+		 return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model model)
+	{
+		 model.addAttribute("title","About - Smart Contact Manager");
+		 return "about";
+	}
+	
+	@RequestMapping("/signup")
+	public String signup(Model model)
+	{
+		 model.addAttribute("title","Register - Smart Contact Manager");
+		 return "signup";
+	}
 }
